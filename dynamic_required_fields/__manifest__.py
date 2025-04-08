4ec0b38de505a284f19adf1f23f038b2fd324ebf
Mitul Bharola <mitul@kandooit.co>
2025-04-08 19:20:33 +0530

    add module in 18.0

---
+++ dynamic_required_fields/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Dynamic Required fields",
+    "description": "Set required fields dynamically",
+    "summary": "Dynamically make fields required based on configuration.",
+    "category": "Tools",
+    "version": "18.0",
+    "price": 5,
+    "currency": "EUR",
+    "author": "CodeCrafters",
+    "website": "https://www.codecrafters.in",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/access_required_fields.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "license": "LGPL-3",
+}
