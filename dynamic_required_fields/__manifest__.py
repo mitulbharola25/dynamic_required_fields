23b92829373228d4db3c5aa15ee9b65592417a3b
Mitul Bharola <122095873+mitulbharola25@users.noreply.github.com>
2025-04-09 13:34:04 +0530

    Update __manifest__.py
    Add manifest changes

--- dynamic_required_fields/__manifest__.py
+++ dynamic_required_fields/__manifest__.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Dynamic Required fields",
-    "description": "Set required fields dynamically",
+    "description": """Set required fields dynamically""",
     "summary": "Dynamically make fields required based on configuration.",
     "category": "Tools",
     "version": "16.0",
     "price": 5,
     "currency": "EUR",
     "author": "CodeCrafters",
-    "website": "https://www.codecrafters.in",
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
         "views/access_required_fields.xml",
+    ],
+    'images': [
+        'static/description/banner.png',
     ],
     "installable": True,
     "auto_install": False,
